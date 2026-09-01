2c1ce993418732a772665477e8969abbf920fba6
jpgallagher1 <jpgallagher@gmail.com>
2026-09-01 13:13:00 -0700

    typo in the chmc_gmm taus specification

--- CHMC/scripts/gmm/chmc_gmm_AA.py
+++ CHMC/scripts/gmm/chmc_gmm_AA.py
@@ -49,7 +49,7 @@ n_runs = 10
 
 ### time integration parameters
 # taus = 2**-jnp.linspace(1, 4, 4)
-taus = jnp.linspace(0.11, 0.8, 7) # extended sweep of sub region
+taus = jnp.linspace(0.11, 0.08, 7) # extended sweep of sub region
 Ts = jnp.linspace(1, 5, 5)
 lens = jnp.logspace(2, 4, 9, base=10, dtype=int)
 
